540bdaf7b8df4002fd7dab2fedb26e0cffc03b60
lynxrelax <lynxrelax@yahoo.com>
2021-11-05 23:40:31 -0400

    1hr at OOP

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+
+import csv
 class Item:
     pay_rate = 0.8 # the pay rate after 20% sale
+
+    all = []
+
     def  __init__(self,name:str,price:float,quantity=0):
         #run validations to the received arguments
         assert price >=0, f"Price {price} is not greater than or equal to zero!"
@@ -8,19 +13,35 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+        # actions to execute
+        Item.all.append(self)
 
     def calculate_total_price(self):
         return self.price *self.quantity
     
     def apply_discount(self):
-        self.price = self.price * Item.pay_rate
+        self.price = self.price * self.pay_rate
 
- 
-item1 = Item('Phone',100,5)
-item2 = Item('Laptop',1000,3)
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open ('items.csv','r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+        for item in items:
+            Item(
+                name = item.get('name'),
+                price = float(item.get('price')),
+                quantity = int(item.get('quantity')),
+            )
 
-item1.apply_discount()
 
-print(item1.price)
+    def __repr__(self):
+        return f"Item('{self.name}',{self.price},{self.quantity})"
+    
 
-input()
+
+Item.instantiate_from_csv()
+print(Item.all)
+
+# for instance in Item.all:
+#     print(instance.name)
