286fec5554a5d8e337ad04b57a52ce84a6d985b0
lynxrelax <lynxrelax@yahoo.com>
2021-11-08 22:05:55 -0500

    save for now

---
+++ Automation/news_scraper.py
@@ -0,0 +1,33 @@
+import requests # http requests
+
+from bs4 import BeautifulSoup # web scraping
+# Send the mail
+import smtplib
+# email body
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+# system date and time manipulation
+import datetime
+
+
+now = datetime.datetime.now()
+
+# email content placeholder
+
+content = ''
+
+
+#extracting Hacker News Stories
+
+
+def extract_news(url):
+    print('Extracting Hacker News Stories...')
+    cnt = ''
+    cnt +=('<b>HN Top Stories:</b>\n'+'<br>'+'-'*50+'<br>')
+    response = requests.get(url)
+    content = response.content
+    soup = BeautifulSoup(content,'html.parser')
+    for i,tag in enumerate(soup.find_all('td',attrs={'class':'title','valign':''})):
+        cnt += ((str(i+1)+' :: '+tag.text + "\n" + '<br>') if tag.text!='More' else '')
+        #print(tag.prettify) #find_all('span',attrs={'class':'sitestr'}))
+    return(cnt)
--- Automation/redme.md
+++ Automation/redme.md
@@ -1,1 +1,5 @@
 for another turtorial
+https://www.youtube.com/watch?v=s8XjEuplx_U&list=PLKlruFVjVc1Lld_-7mIpoNGdcxjaXHd9O&index=12&t=704s
+
+repo
+https://github.com/amrrs/build_tools_to_automate_python
