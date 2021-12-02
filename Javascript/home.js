d5093b5e2459b8030751178da1e90a2053fd1f91
lynxrelax <lynxrelax@yahoo.com>
2021-12-01 21:04:04 -0500

    start js full course

---
+++ Javascript/home.js
@@ -0,0 +1,19 @@
+// document.getElementById("count-el").innerText = 5
+
+let countEL = document.getElementById("count-el")
+let count = 0
+function increment(){
+  count ++
+  countEL.innerText = count
+}
+
+
+// function countdown(h) {
+//   var i = h
+//   while (i > 0) {
+//     console.log(i);
+//     i--;
+//   } 
+// }
+
+// countdown(5)
